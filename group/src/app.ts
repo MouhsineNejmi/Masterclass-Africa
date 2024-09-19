1c06df6749abd5668d5e0f89c942d4038225b6c6
MouhsineNejmi <nejmimouhsine@gmail.com>
2024-09-19 09:51:33 +0100

    init course microservice

---
+++ group/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { errorHandler, NotFoundError } from '@nmasterclass-africa/common';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
